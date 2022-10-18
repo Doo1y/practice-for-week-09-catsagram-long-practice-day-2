e6d7880d0390fe55c3dc89790892cade5a1ffcd7
Samuel Park <s.park111497@gmail.com>
2022-10-17 17:23:28 -0700

    finished day-2 of the project

---
+++ public/comments.js
@@ -0,0 +1,32 @@
+export const createCommentsForm = () => {
+  const commentDiv = document.createElement("div");
+  commentDiv.className = "form form-child";
+
+  const labelEl = document.createElement("label");
+  labelEl.htmlFor = "comment";
+  labelEl.innerText = "Comment:";
+  
+  const textArea = document.createElement("input");
+  textArea.type = "text";
+  textArea.placeholder = "Add a comment...";
+
+  const submitBtn = document.createElement("input");
+  submitBtn.type = "submit";
+
+  const commentsList = document.createElement("ul");
+  commentsList.id = "comments-list";
+
+  document.querySelector("#comments-form").addEventListener("submit", event => {
+    if (textArea.value.length <= 0) alert("You need to add a comment to submit!");
+    else {
+      const comment = document.createElement("li");
+      comment.innerText = document.querySelector("input[type='text']").value
+      commentsList.appendChild(comment);
+      document.querySelector("input[type='text']").value = ""
+    }
+    event.preventDefault();
+  })
+
+  commentDiv.append(labelEl, textArea, submitBtn);
+  document.querySelector("#comments-form").append(commentDiv, commentsList)
+}
--- public/index.js
+++ public/index.js
@@ -1,17 +1,33 @@
-import { createMainContent } from './main.js';
+import { createMainContent, fetchNewImage, createVoteElements } from './main.js';
+import { createCommentsForm } from "./comments.js"
 
 const initializePage = () => {
-    // Create container
-    const container = document.createElement("section");
-    container.className = "container";
-    container.style.display = "flex";
-    container.style.flexDirection = "column";
-    container.style.alignItems = "center";
-    container.style.marginTop = "20px";
-    document.body.appendChild(container);
+  // Create container
+  const container = document.createElement("main");
+  container.className = "container";
+  container.style.display = "flex";
+  container.style.flexDirection = "column";
+  container.style.alignItems = "center";
+  container.style.marginTop = "20px";
+
+  const imageBox = document.createElement("section");
+  imageBox.className = "image-box";
+
+  const formElement = document.createElement("form");
+  formElement.id = "comments-form";
+  formElement.className = "form";
+  formElement.method = "post";
+
+  container.append(imageBox, formElement);
+  document.body.append(container);
+  
 };
 
 window.onload = () => {
-    initializePage();
-    createMainContent();
+  initializePage();
+  createMainContent();
+  createVoteElements();
+  createCommentsForm();
+  fetchNewImage();
+
 };
